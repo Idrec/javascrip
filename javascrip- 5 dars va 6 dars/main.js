c27cd0c564a5dfca175b2a2e4563ce4315ea63a1
Idrec <narzievuktam@gmail.com>
2025-04-14 11:03:49 +0500

    Add files via upload

---
+++ javascrip- 5 dars va 6 dars/main.js
@@ -0,0 +1,732 @@
+// let a = prompt("son kirit: ")
+// let b = prompt("username: ")
+// let c = prompt("parol")
+// if(a>0 && b!=='admin' && c!=='parol'){
+//     alert('musbat')
+// }else if(a<0 && b!='admin' && c!=='parol'){
+//     alert('manfiy')
+// }else{
+//     alert('nolga teng')
+// }
+
+// let x = 1
+// let y = 1
+// let z = 5
+// if((x+y+z)>x*y*z){
+//     alert("max")
+// }else if((x*y*z)>x+y+z){
+//     alert('min')
+// }else{
+//     alert('x=y')
+// }
+
+
+// let x = 1
+// let y = 1
+// let z = 1
+// if(((x+y+z/2))>x*y*z){
+//     alert("max")
+// }else if(((x+y+z/2)<x*y*z)**2+1){
+//     alert("min")
+// }else{
+//     alert("nolga teng")
+// }
+
+// let a = 5
+// let b = 3
+// let c = 1
+// if(a >= b && a >= c){
+//     alert('a')
+// }else if(b <= a && b >= c){
+//     alert("b")
+// }else if(c < a && c <= b){
+//     alert('c')
+// }else{
+//     alert("noto'g'ri tuzilgan")
+// }
+
+// let a = 1
+// let b = 3
+// let c = 1
+// if((a >= b >= c)*2){
+//     alert('a')
+// }else if(a != b != c || a | 5){
+//     alert("b")
+// }else{
+//     alert("noto'g'ri tuzilgan")
+// }
+
+
+// let x = 1
+// let y = 2
+// if(x>y){
+//     alert("z=x-y")
+// }else{
+//     alert("y-x+1")
+// }
+
+// let a = 4
+// let b = 2
+// if(a > b){
+//     alert(a)
+// }else {
+//     alert(a , b)
+// }
+
+// let a = 7
+// let b = 2
+// if(a <b || a==b){
+//     alert(a)
+// }else{
+//     a=0
+//     alert(a)
+// }
+
+// let a = 12
+// let b = 9
+// let c = 7
+// if(a/3 && a/1 && b/3 && b/1 && c/3 && c/1){
+//     alert('butun sonlar')
+// }
+// 10 masala
+// let x = 8
+// let y = 4
+
+// if(x>y){
+//     x =(x+y)/2
+//     alert(x)
+// }else if(x<y){
+//     x = (x*2)
+//     alert(x)
+// }
+
+// 11 masala
+
+// let a = 8
+// let b = 10
+// let c = 4
+// if(a > 0 && b > 0 && c > 0){
+//     a = a**2
+//     b = b**2
+//     c = c**2
+//     console.log (a)
+//     console.log (b)
+//     console.log (c)
+// }else if(a < 0 && b > 0 && c > 0){
+//     a = a**2
+//     b = b**2
+//     c = c**2
+//     console.log (b)
+//     console.log (c)
+// }else if(a > 0 && b < 0 && c > 0){
+//     a = a**2
+//     b = b**2
+//     c = c**2
+//     console.log (a)
+//     console.log (c)
+// }else if(a > 0 && b > 0 && c < 0){
+//     a = a**2
+//     b = b**2
+//     c = c**2
+//     console.log (a)
+//     console.log (b)
+// }
+
+// 12 masala
+// let a = 1
+// let b = 2
+// let c = 3
+// let d = 4
+// if(d >= c && d>=b && d >= a){
+//     d = c
+//     d = b
+//     d = a
+//     console.log (d)
+//     console.log (c)
+//     console.log (b)
+//     console.log (a)
+// }else if(a >= b >= c >= d){
+//     console.log (a)
+//     console.log (b)
+//     console.log (c)
+//     console.log (d)
+// }else if(a <= b <=c <= d){
+//     a=a**2
+//     b=b**2
+//     c=c**2
+//     d=d**2
+//     console.log (a)
+// }
+
+// 13 masala
+// let x = prompt("tenglama:(((a*x)**2)+(b*x)+c)**0.5 == 0, x uchun son kiriting: ")
+// let a =  prompt("tenglama:(((a*x)**2)+(b*x)+c)**0.5 == 0 a uchun son kiriting: ")
+// let b =  prompt("tenglama:(((a*x)**2)+(b*x)+c)**0.5 == 0 b uchun son kiriting: ")
+// let c =  prompt("tenglama:(((a*x)**2)+(b*x)+c)**0.5 == 0 c uchun son kiriting: ")
+// console.log((((a*x)**2)+(b*x)+c)**0.5 == 0)
+
+// 14 masala
+// let x = 10
+// let y = 10
+// let a = prompt("son kiriting; ")
+// let c = prompt('son kiriting: ')
+// let b = 10
+// if(x==a && y == c){
+//     alert('gisht sigadi')
+// }else if(x > a && y > c || x < a && y < c){
+//     alert("gisht sigmaydi")
+// }
+
+
+
+// let a = prompt('son kiriting: ')
+// let b = prompt('son kiriting: ')
+// let c = prompt('son kiriting: ')
+// let d = prompt('son kiriting: ')
+// let s = 0
+// while(a > 0){
+//     s+=a
+//     console.log (s)
+// }
+
+// let i =0
+
+// while(i<10){
+
+//     let a= prompt('son kirit: ')
+//     if(a%2==0){
+//         console.log(`juft ${a}`)
+//     }else{
+//         console.log(`toq ${a}`)
+
+//     }
+//     i++
+// }
+
+
+
+
+
+//   5 -dars
+// let a = 50
+// for(let i = 1; i < 50; i++){
+//     if(i<=a){
+//         i == 7 ;false;
+//         continue
+//     }
+//     console.log (i)
+
+// }
+// 1misol
+// let a = [1, 3, -1, 4, -4]
+// let k = 0
+// for (let b of a) {
+//     if (b > 0) {
+//         // console.log(b) 
+//         k += b
+
+//     }
+// }
+// console.log(k)
+
+// 2misol
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let k = 0
+// for (let b of a){
+//     if (b % 2 == 0)
+//         console.log(b)
+// }
+// 3misol
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let i = 0; 
+// for (let b of a) {
+//     if (i <= b) {
+//         i++
+//     }
+// }
+// console.log(i)
+
+// 4misol
+// let a = [1, 2, 3, 4, 5];
+// let c = [];
+// for (let b of a) {
+//     c.push(b * 2);
+// }
+// console.log(c)
+
+
+
+// 5misol
+// let massiv = [3, 6, -5, 22, -3, 9, 11, -2, 5, 7, 0, 8];
+
+// for (let i = massiv.length - 1; i >= 0; i--) {
+//     console.log(massiv[i]);
+// }
+
+// 6 mishol
+// let a = [1, 2, 3, 4, 5];
+// let c = [];
+// for (let b of a) {
+//     c.push(b ** 2);
+// }
+// console.log(c)
+
+// 7misol
+
+// let a = [1, 2, 3, 4, 5,10,15];
+// for(b of a){
+//     if(b % 5 == 0){
+//         console.log(b)
+//     }
+// }
+
+// 8misol
+
+// let a = [1, 2, 3, 4, 5,10,15];
+// for(key in a){
+// }
+// for(b of a){
+// }
+// console.log(key, a)
+
+// 9misol
+// let a = [1,2,3]
+// let b = [4,5,6]
+// let c = []
+// for(let i = 0; i< a.length; i++){
+//     c.push(a[i]+b[i])
+// }
+// console.log(c)
+
+// 10 misol
+// let a = [1, 2, 3, 4, 5,10,15];
+// let n = +prompt('son kiriting: ')
+// for(c of a){
+//     if(c > n){
+//         console.log(c) 
+//     }
+
+// }
+
+// 11 misol
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let i = 0; 
+// for (let b of a) {
+//     if (i >= b) {
+//         i++
+//     }
+// }
+// console.log(i)
+
+
+// let massiv = [120, 3, -2, 15, 7, 10, 5, 8, -1, 65, 105];
+// let engKatta = massiv[0];
+
+// for (let i = 1; i < massiv.length; i--) {
+//     if (massiv[i] > engKatta) {
+//         engKatta = massiv[i];
+//     }
+// }
+// console.log("Massivdagi eng katta element:");
+
+// 14 misol
+
+// const massiv = [1,2,4,5,5,5]
+// const a = []
+// let count = 0
+// let k = 5
+// for(let i = 0; i < massiv.length; i++){
+//   for(b of massiv){
+//     if(b != k){
+//       a.push(b)
+//       console.log(b)
+//     }
+//   } 
+// }
+
+
+// 15 masala
+
+// let  a = [1,2,3,4,5]
+// let b = 0
+// for(let i = 0;i<a.length;i++){
+//     b+=a[i]
+// }
+// let c = b/a.length
+// let d = []
+// for(g of a){
+//     if(c<g){
+//         d.push(g)
+//     }
+// }
+// console.log(d)
+
+
+// 16 masala
+
+// let  a = [1,2,3,4,5]
+// g = []
+// u = []
+// for (b of a){
+//     if(b % 2 == 0){
+//         u.push(b)
+//     }
+// }
+// console.log(u)
+
+// for(b of a){
+//     if(b % 2){
+//         g.push(b)
+//     }
+// }
+// console.log(g)
+
+
+// 17masala
+
+// let  a = [1,2,3,4,5]
+// let b = []
+// let d = []
+// for(c of a){
+//     if(c <)
+// }
+// for(i = 0; i<a.length; i++){
+
+// }
+
+// let massiv = [10, 15, 25, 20, 30, 40];
+
+// let farqlar = [];
+
+// for (let i = 0; i < massiv.length - 1; i++) {
+//     let farq = massiv[i + 1] - massiv[i]; 
+//     farqlar.push(farq); 
+// }
+
+
+// console.log("Massivdagi elementlar orasidagi farqlar:", farqlar);
+
+
+// 18-masala
+// let a = [-1,2,3,-4,5,-6]
+// let b = []
+// let c = []
+// for(r of a){
+//     if(r<0){
+//         b.push(r)
+//     }
+// }
+// console.log(b)
+// for(g of a){
+//     if(g>0){
+//         c.push(g)
+//     }
+// }
+// console.log(c)
+
+// 19masala
+
+// const a = [1, 2, 3, 4, 5, 6, 7];
+// let b = [];
+// for (let i = a.length - 0; i >= 0; i--) {
+//     b.push(i);
+//     console.log(b);
+// }
+
+
+
+
+// // 20.masala
+
+// let massiv = [10, 15, 25, 20, 30, 40];
+// let farqlar = [];
+// for (let i = 1; i < massiv.length; i++) {
+//     let farq =massiv[i] - massiv[i - 1]; 
+//     farqlar.push(farq); 
+// }
+// console.log(farqlar)
+
+
+
+
+
+
+// 1masala
+
+// let n = parseInt(prompt("n ni kiriting:"));
+
+// let massiv = [];
+// let son = 1;
+
+// for (let i = 0; i < n; i++) {
+//     massiv.push(son);
+//     son += 2;
+// }
+
+// console.log("Dastlabki " + n + " ta toq son:", massiv);
+
+
+// 2masala
+
+// let n = parseInt(prompt("n ni kiriting:"));
+
+// let massiv = [];
+// let son = 1;
+
+// for (let i = 0; i < n; i++) {
+//     massiv.push(son);
+//     son *= 2;
+// }
+
+// console.log("Dastlabki " + n + " ta toq son:", massiv);
+
+
+// 3 masala
+// let c = []
+// let n = prompt('son kiriting: ')
+// let a = 10
+// let d = 5
+// for( let i = 0; i <= n; i++){
+//     a+=d
+//     c.push(a)
+// }
+// console.log(c)
+
+
+// 4masala
+// let c = []
+// let n = prompt('son kiriting: ')
+// let a = 10
+// let d = 5
+// for( let i = 0; i <= n; i++){
+//     a*=d
+//     c.push(a)
+// }
+// console.log(c)
+
+// 6masala
+
+
+// 
+// const a = [20,15,10,50]
+// const b = []
+// for(g of a){
+
+// }
+// for(let i = 0;i < a.length;i++){
+//     const num = a[i];
+//     if(num % 2 ===0){
+//         const c = num*num
+//         if(c<100){
+//             b.push(c)
+//         }
+//         console.log(b)
+//     }
+// }
+// console.log(b)
+
+// 
+
+// const numbers = [3, 8, 12, 5, 10, 7, 14, 9, 11, 6];
+// const result = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   const num = numbers[i];
+//   if (num % 2 === 0) {              // Juft sonligini tekshiramiz
+//     const squared = num * num;      // Kvadratga ko'taramiz
+//     if (squared < 100) {            // 100 dan kichikligini tekshiramiz
+//       result.push(squared);         // Natijaga qo'shamiz
+//     }
+//   }
+// }
+
+// console.log("Natija (if bilan):", result);
+
+// let a = [5,89,77,55,10,7,6]
+// let c = 50
+// let b = []
+// for(let i of a){
+//     if(c>i){
+//         b.push(i)
+//     }
+// }
+// b.sort()
+// console.log(b)
+
+
+
+// let a = ['ab',77,'joxa',55,'bor',10,6]
+// let g = []
+// // let p = String(true)
+// for (let b of a){
+//     // console.log(typeof(b))
+//      if(typeof(b) == 'string'){
+//         g.push(b)
+//         // console.log(b)
+//      }
+// }
+// g.sort()
+// console.log(g)
+
+
+
+
+// let a = [77,77,55,6,55,9]
+// let g = []
+// for (let i of a){
+//     if(g.includes(i)){
+
+//     }else{
+//         g.push(i)
+//     }
+// }
+
+
+
+// console.log(g)
+// let g = [12,1,3,33,10]
+// let d = 0 
+// let j = 0
+// let a = prompt('son kirit: ')
+// for(let i of g){
+//     if(a<i){
+//         d++
+//         j+=i
+//     }
+// }
+// console.log(j/d)
+// console.log(d)
+
+
+// let arr = [-1, -2, -3, 4,9, -5, -6];
+
+// let minus = arr.filter(num => num < 0).length;
+
+// if (minus > 3) {
+//     arr = arr.map(num => num < 0 ? 0 : num);
+// }
+// console.log( minus);
+// console.log( arr);
+
+// 11masala
+// let a = [30,40,30,33,10,1,10]
+// let d = []
+// a.sort
+// alert(a.slice(-2))
+
+// 13masala
+// let a = [30, -40, 30, -33, 10, -10]
+// b = []
+// p = []
+// for (let s of a) {
+//     if (s <= 0) {
+//         b.push(s)
+//     }
+// } console.log(b)
+// for (let h of a){
+//     if (h > 0) {
+//         p.push(h)
+//     }
+// }console.log(p)
+
+
+// 15masala
+// let arr = [1, 2, [1, 2], 3, 4, 5];
+// let flatArr = arr.flat();
+// let result = flatArr.reduce((sum, current) => sum + current);
+
+// console.log(result);
+
+// 16masala
+// let a = [1, 2, 3, 4, 5];
+// let b = []
+// for(let i in a){
+//     if(i%2 != 0){
+//         b.push(i)
+//     }
+// }
+// console.log(b)
+
+
+// 17masala
+// let a = [0,10,20,40]
+// let b = [1,15,20,0]
+// let c = []
+// for (let i of a){
+//     if(b.includes(i)){
+//         c.push(i)
+//     }
+// }console.log(c)
+
+// 18-masala
+// let b = ["aziz", "ali", "alovuddin", 1, 3];
+// let sum = 0;
+
+// for (let i = 0; i < b.length; i++) {
+//     if (typeof b[i] === "string") {
+//         sum += b[i].length;
+//     }
+// }
+
+// console.log(sum);
+
+
+// 19masala
+// let i = 0
+// let b = []
+// while(i<5){
+//     i++
+// a = +prompt('son kiritin: ')
+// b.push(a)
+// }console.log(b.sort()[b.length -1])
+// let c = []
+// for(let i = 0; i<5;i++){
+//     let a = +prompt('son')
+//     if (c.length==0){
+//         c.push(a)
+//     }else if(c[0]<a){
+//         c[0]=a
+//     }
+// }console.log(c[0])
+// let qiymat = []
+
+
+
+
+// for (let i = 0; i<5; i++){
+//     let qiymatlar = +prompt(Iltimos, ${i+1}- Qiymatni Kiririting)
+//     if(isNaN(qiymatlar)){
+//         alert("Raqam Kiriting")
+//         i--;
+//         continue;
+//     }
+//     qiymat.push(qiymatlar)
+// }
+
+// console.log(qiymat);
+// console.log(Math.max(...qiymat))
+
+
+
+
+
+
+// FUNKSIYALAR
+
+// function MaxNumber(c=[]){
+//     for(let i = 0;i<5;i++){
+//         let a = +prompt('son: ')
+//         if(c.length==0){
+//             c.push(a)
+//         }else if(c[0]<a){
+//             c[0]=a
+//         }
+//     }
+
+//     console.log(c[0])
+// }
+
+// MaxNumber()
+
+0
