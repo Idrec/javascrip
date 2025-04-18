45b43d3661a7781141faa7d687e90b2c2315b2ef
Idrec <narzievuktam@gmail.com>
2025-04-18 11:08:31 +0500

    Add files via upload

---
+++ jc 10 dars/java-darslar.js
@@ -0,0 +1,102 @@
+// let arr = ["I", "go", "home"];
+
+// // delete arr[1]; // удалить "go"
+// arr.splice(1, 2); // начиная с индекса 1, удалить 1 элемент
+
+
+
+// // console.log(d)
+
+
+
+
+// let arr = ["t", "e", "s", "t"];
+
+// alert( arr.slice(1, 3) ); // e,s (копирует с 1 до 3)
+
+// alert( arr.slice(-2) ); // s,t (копирует с -2 до конца)
+
+
+// let b = [3, 4]
+// let c = [5, 6]
+// let arr = []
+// alert( arr.concat(b,c) ); // 1,2,3,4,5,6
+
+
+
+
+// let arr = [1, 0, false];
+
+// alert( arr.indexOf(0) ); // 1
+// alert( arr.indexOf(false) ); // 2
+// alert( arr.indexOf(null) ); // -1
+
+// alert( arr.includes(-1) ); // true
+
+
+
+// let fruits = ['Яблоко', 'Апельсин', 'Яблоко']
+
+// alert( fruits.indexOf('Яблоко') ); // 0 (первый 'Яблоко')
+// alert( fruits.lastIndexOf('Яблоко') ); // 2 (последний 'Яблоко')
+
+
+
+// let lengths = ["Бильбо", "Гэндальф", "Назгул"].map(item => item.length);
+// alert(lengths); 
+
+
+
+// let arr = [ 1, 20, 15,3 ];
+
+// arr.sort();
+
+// alert( arr );  
+
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.reverse();
+
+// alert( arr ); // 5,4,3,2,1
+
+
+
+// let names = 'Вася, Петя, Маша';
+
+// let arr = names.split(', ',1);
+
+// console.log(arr)
+
+
+
+// let arr = ['Вася', 'Петя', 'Маша'];
+
+// let str = arr.join(';'); // объединить массив в строку через ;
+
+// alert( str ); // Вася;Петя;Маша
+
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let result = arr.reduce((sum, current) => sum + current,5);
+
+// alert(result); // 15
+
+
+
+
+
+
+// function MaxNumber(c=[]){
+//     for(let i = 0;i<5;i++){
+//         let a = +prompt('son: ')
+//         if(c.length==0){
+//             c.push(a)
+//         }else if(c[0]<a){
+//             c[0]=a
+//         }
+//     }
+
+//     console.log(c[0])
+// }  funksiya uchun namuna
